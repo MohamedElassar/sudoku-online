7b718b80b533b3825bd80b7fca8c4fb144c3586f
Mohamed Elassar <61672955+MohamedElassar@users.noreply.github.com>
2021-01-23 21:26:39 -0500

    Update 1
    Added buttons that fetch puzzles from an API and update the puzzle accordingly. Need to fix the visuals + implement a solving mechanism

--- application.js
+++ application.js
@@ -1,39 +1,31 @@
 const puzzle_HTML = document.getElementById("puzzle");
+const easy_button_HTML = document.getElementById("easy");
+const medium_button_HTML = document.getElementById("medium");
+const hard_button_HTML = document.getElementById("hard");
+
+easy_button_HTML.addEventListener("click", setURL);
+medium_button_HTML.addEventListener("click", setURL);
+hard_button_HTML.addEventListener("click", setURL);
+
+var url = "https://sugoku.herokuapp.com/board?difficulty=";
 
 class Square {
-/*
-	constructor(id){
-		this.button = document.createElement("button");	
-		this.button.setAttribute('class', 'buttons');
-		this.button.setAttribute('id', id);
-		this.button.innerHTML = "";
-	}
-
-	setValue(value){
-		this.button.innerHTML = value;
-	}
-	
-	getButton(){
-		return this.button;
-	}
-*/
-
 	constructor(id)	{
 		this.inp = document.createElement("input");	
-		this.inp.setAttribute('class', 'buttons');
+		this.inp.setAttribute('class', 'inputs');
 		this.inp.setAttribute('id', id);
-		this.inp.innerHTML = "";
+		this.inp.value = "";
 	}
 
 	setValue(value){
-		this.inp.innerHTML = value;
+		if(value !== 0){
+			this.inp.value = value;
+		}
 	}
 	
 	getButton(){
 		return this.inp;
 	}
-	
-
 }
 
 class Grid {
@@ -41,72 +33,129 @@ class Grid {
 		this.row = row;
 		this.col = col;
 		this.stats = Array.from(Array(this.row), () => new Array(this.col));
-		this.squares = [];
+		this.squares = [];	
+		this.initializeSquaresArray();
+	}
+
+	initializeSquaresArray(){
 		for(let i = 0; i < this.row; i++){
 			var temp = [];
 			for(let j = 0; j < this.col; j++){
 				let unique_id = this.row * i + j;
 				let tempSquare = new Square(unique_id);
-				tempSquare.getButton().addEventListener("input",
-					this.handleClicks);
+				tempSquare.getButton().addEventListener("input",this.handleClicks);
 				temp.push(tempSquare);
 			}
 			this.squares.push(temp);
 		}
 	}
-	
+
+
+	beginNewArray(newArray) {
+		this.clearGrid();
+		for(let i = 0; i < this.row; i++){
+			for(let j = 0; j < this.col; j++){
+				this.squares[i][j].setValue(newArray[i][j]);
+			}
+		}
+	}
+
+
+	clearGrid() {
+		for(let i = 0; i < this.row; i++){
+			for(let j = 0; j < this.col; j++){
+				this.squares[i][j].setValue("");
+			}
+		}		
+	}
+
 	handleClicks(){
 		this.style.backgroundColor = "red";
 		console.log(this.id);
 	}
 
 	updateSquares(stats){
-		for(let i = 0; i < this.row; i++){
-			for(let j = 0; j < this.col; j++){
-				this.squares[i][j].setValue(stats[i][j]);
-			}
-		}
+
 	}
 
 
 	generateGrid(){
+
 		let table = document.createElement("table");
-		table.setAttribute('class', 'puzzle');
+
 		for(var i = 0; i < this.row; i++){
+
 			var div = document.createElement("tr");
 			div.setAttribute('id', 'row-' + i);
 			div.setAttribute('class', 'row');
-			for(var j = 0; j < this.col; j++){
-				let entry = document.createElement("td");
-				entry.appendChild(this.squares[i][j].getButton());
-				div.appendChild(entry);
-			}
-			puzzle_HTML.appendChild(div);
-		}
-	}
-
-}
-
-
 
 /*
-	generateGrid(){
-		for(var i = 0; i < this.row; i++){
-			var div = document.createElement("div");
-			div.setAttribute('id', 'row-' + i);
-			div.setAttribute('class', 'row');
-			for(var j = 0; j < this.col; j++){
-				let button_div = document.createElement("div");
-				button_div.setAttribute('class', 'buttons');
-				button_div.appendChild(this.squares[i][j].getButton());
-				div.appendChild(button_div);
+			if(i%3 === 0) {
+				div.style.borderTop = '3px solid red';
+			} else if (i === this.row - 1) {
+				div.style.borderBottom = '3px solid red';
 			}
-			puzzle_HTML.appendChild(div);
+*/
+
+			for(var j = 0; j < this.col; j++){
+				var entry = document.createElement("td");
+				entry.appendChild(this.squares[i][j].getButton());
+
+				if(j%3 === 0) {
+					entry.style.borderLeft = '3px solid #747C7B';
+				} else if (j == this.col - 1) {
+					entry.style.borderRight = '3px solid #747C7B';
+				}
+
+				if(i%3 === 0) {
+					entry.style.borderTop = '3px solid #747C7B';
+				} else if (i == this.row - 1) {
+					entry.style.borderBottom = '3px solid #747C7B';
+				}
+
+/*
+				if(j%3 === 0) {
+					div.style.borderLeft = '3px solid red';
+				} else if (j === this.col - 1) {
+					div.style.borderRight = '3px solid red';
+				}
+*/
+
+				div.appendChild(entry);
+			}
+			table.appendChild(div);
+			//puzzle.appendChild(div);
 		}
+		puzzle.appendChild(table);
 	}
 
 }
-*/
+
+
+
+function setURL(){
+	var temp_url = url + this.innerHTML;
+	console.log(temp_url);
+	getPuzzle(temp_url);	
+}
+
+
+function getPuzzle(temp_url){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", temp_url, true);
+	xmlhttp.send();		
+
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+	    		var str = this.responseText;
+			var json = JSON.parse(str);
+			grid.beginNewArray(json["board"]);
+
+		}
+	};
+}
 
 const grid = new Grid(9, 9);
 grid.generateGrid();
+
+
