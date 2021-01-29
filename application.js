5b78138d19455d0a03bb5b1820d46b3daab499b6
Mohamed Elassar <mohamedelassar1997@gmail.com>
2021-01-29 14:35:36 -0500

    Comments
    Added lots of comments + made some slight changes in each file

--- application.js
+++ application.js
@@ -13,6 +13,16 @@ var url = "https://sugoku.herokuapp.com/board?difficulty=";
 const ROWS = 9;
 const COLS = 9;
 
+/*
+The class Square is used to create an Input element at each cell in the sudoku table
+Even though an input element is created for every cell in the puzzle, cells with
+fixed values forming the puzzle to be solved are set to readonly.
+
+The value for the input elements is updated when the user enters a number. An eventListener
+is added to each input element to perform some checks on the input, ensuring that it is an integer
+falling between 1 and 9, inclusive. The callback function for this event listener is handleClicks,
+which can be found in the Grid class.
+*/
 class Square {
 	constructor(id) {
 		this.inp = document.createElement("input");	
@@ -20,15 +30,10 @@ class Square {
 		this.inp.setAttribute('type', 'number');
 		this.inp.setAttribute('min', 1);
 		this.inp.setAttribute('max', 9);
-		//this.inp.setAttribute('maxlength', 1);
 		this.inp.setAttribute('id', id);
 		this.inp.setAttribute('readonly', true);
 	}
 	
-	getValue(){
-		return this.inp.value;
-	}
-
 	setReadOnly(bool){
 		if(bool) {
 			this.inp.setAttribute('readonly', bool);	
@@ -43,15 +48,28 @@ class Square {
 }
 
 class Grid {
+	/*
+	Initializing a grid instance. Square instances are created and are stored in the 2D array
+	this.squares. this.squares[0] would represent the Square elements in the first row of the 
+	grid, and this.squares[0][0] would represent the Square element in the top left corner of the grid.
+
+	this.puzzle is an array that stores the solved version of the sudoku puzzle. The solution is obtained
+	through the getSol method of this class, which calls functions in the file solver.js.
+	*/ 
 	constructor(row, col){
 		this.row = row;
 		this.col = col;
-		this.stats = Array.from(Array(this.row), () => new Array(this.col));
 		this.squares = [];	
 		this.initializeSquaresArray();
 		this.puzzle = [];
 	}
 
+	/*
+	This method initializes this.squares by creating i*j instances of the Squares class and
+	storing them in the arary. Event listener is added to each Square element that handles the change
+	in input by calling this.handleClicks. This method ensures that the value entered by the user is 
+	a valid entry, and updates the UI based on that result. 
+	*/
 	initializeSquaresArray(){
 		for(let i = 0; i < this.row; i++){
 			var temp = [];
@@ -65,11 +83,15 @@ class Grid {
 		}
 	}
 
-
+	/*
+	Callback function to the event listener associated with the Squares elements. 
+	Ensures that the input value is a valid entry between 1 and 9
+	If the input is not valid, an alert message is displayed to the user and the value of the
+	input element is restored to an empty string
+	*/
 	handleClicks(){
 		if(this.value >= 1 && this.value <= 9){
 			this.style.color = '#679D64';
-			//this.style.color = 'white';
 		} else {
 			alert("Please enter a number between 1 and 9, inclusive");
 			this.value = "";
@@ -77,7 +99,14 @@ class Grid {
 				
 	}
 
-
+	/*
+	This method updates the grid by adding the values obtained from the puzzle. The value of each Square element
+	in this.squares is updated to the corresponding value from the puzzle. If the corresponding
+	value is 0 (meaning that this is an unknown field that the user should input), the Square element's
+	readonly attribute is set to false, meaning that the user can edit this field. If the corresponding value
+	is a that is part of the puzzle and isn't supposed to change, the square element's readonly
+	attribute is set to true 
+	*/
 	beginNewArray(newArray) {
 		for(let i = 0; i < this.row; i++){
 			for(let j = 0; j < this.col; j++){
@@ -92,9 +121,7 @@ class Grid {
 		}
 	}
 
-
 	generateGrid(){
-
 		let table = document.createElement("table");
 
 		for(var i = 0; i < this.row; i++){
@@ -105,6 +132,11 @@ class Grid {
 
 			for(var j = 0; j < this.col; j++){
 				var entry = document.createElement("td");
+				
+				var c = document.createElement("label");
+				c.setAttribute('for', i*ROWS + j);
+				entry.appendChild(c);
+
 				entry.appendChild(this.squares[i][j].getButton());
 
 				if(j%3 === 0) {
@@ -126,7 +158,6 @@ class Grid {
 		puzzle.appendChild(table);
 	}
 
-
 	setSol(puzzle_copy){
 		for(let i = 0; i < ROWS; i++){
 			var temp = [];
@@ -137,6 +168,13 @@ class Grid {
 		}
 	}
 
+	/* 
+	This method is executed when the Verify button is clicked. This method will loop through the grid
+	and compare each value against the correct value stored in this.puzzle (which stores the solved version
+	of the puzzle). If the user's value is correct, no change is made to the element. If the user
+	inputted an incorrect value, the background of the element is changed to pink indicating that this is an
+	incorrect entry. If no discrepancies are found, an alert message appears saying "Good Job!"
+	*/
 	verify(){
 		let ctr = 0;
 		for(let i = 0; i < ROWS; i++){
@@ -156,17 +194,13 @@ class Grid {
 
 	}
 
-
 }
-
-
 
 function setURL(){
 	var temp_url = url + this.innerHTML;
 	console.log(temp_url);
 	getPuzzle(temp_url);	
 }
-
 
 function getPuzzle(temp_url){
 
@@ -197,8 +231,6 @@ function getPuzzle(temp_url){
 		}
 	};
 }
-
-
 
 const grid = new Grid(ROWS, COLS);
 grid.generateGrid();
--- solver.js
+++ solver.js
@@ -1,3 +1,23 @@
+/*
+Solver.js is a file that is dedicated to finding a solution to the sudoku puzzle that was
+obtained from https://sugoku.herokuapp.com. The functions in this file are called after the 
+puzzle is displayed to the user. This is done to minimize the wait time before the user can
+see the puzzle.
+*/
+
+/*
+A backtracking algorithm is used to find a solution to the puzzle. The getSol(arr) method starts by
+looping through arr (which is the puzzle in a 2D Array structure with empty fields represented as 0).
+If it lands on a field that equates to 0 (meaning that a value needs to be obtained for this field), 
+it will start by making a guess for the possible solution, starting with 1. It then calls the
+isPossible method and passes the guess, the row and column of the field at question, and the whole puzzle.
+The operation of that method is explained in the comments above it. It returns "true" if the guess
+meets the criteria, and "false" otherwise. If true is returned, the guess is a valid one (for now) and 
+that field in the puzzle (arr[i][j]) is set to that guess. The getSol method is then recursively
+called and it repeats all the previous steps using the updated puzzle with the guess added. If at some
+point no valid guesses can be made for a given field, that means that the combination of guesses we chose
+isn't correct and the getSol function resets the incorrect fields to 0, and then recalls itself recursively
+*/
 function getSol(arr){
 	for(var i = 0; i < ROWS; i++){
 		for(var j = 0; j < COLS; j++){
@@ -19,19 +39,36 @@ function getSol(arr){
 	return 0;
 }
 
+/*
+The isPossibleSol method checks if a guess made in getSol is a valid one. It does that by testing
+the solution according to the following sudoku rules:
+1) Each of the 9 rows must not have any duplicate numbers
+2) Each of the 9 columns must not have any duplicate numbers
+3) Each of the 9 "boxes" must not have any duplicate numbers
+Note: An example of a "box" would be the one formed by the intersection of the
+first 3 rows and first 3 columns
+*/
 function isPossibleSol(num, row, col, board){
+	//checking if the guess already exists in its column
 	for(var j = 0; j < COLS; j++){
 		if(board[row][j] == num && j != col){
 			return false;
 		}
 	}
 
+	//checkinf if the guess already exists in its row
 	for(var j = 0; j < ROWS; j++){
 		if(board[j][col] == num && j != row){
 			return false;
 		}
 	}
 
+	/*
+	checking if the guess already exists in its box. This involves comparing the guess with only
+	4 other fields in the box, and not all 8. This is because when we previously checked the 
+	field's row and column (and assuming that these tests passed), we eliminated the other 4 
+	fields in the same box as our guess, leaving us with only 4 fields to check agianst.
+	*/
 	var row_lower_bound;
 	var row_upper_bound;
 	var col_lower_bound;
@@ -40,24 +77,24 @@ function isPossibleSol(num, row, col, board){
 	var row_remainder = row % 3;
 	var col_remainder = col % 3;
 	
-	if(row_remainder === 0){
+	if(row_remainder === 0){ // check if our guess is in row 0, 3, or 6
 		row_lower_bound = row + 1;
 		row_upper_bound = row + 2;
-	} else if(row_remainder === 1) {
+	} else if(row_remainder === 1) { // check if our guess is in row 1, 4, or 7
 		row_lower_bound = row - 1;
 		row_upper_bound = row + 1;
-	} else if(row_remainder === 2) {
+	} else if(row_remainder === 2) { // check if our guess is in row 2, 5, or 8
 		row_lower_bound = row - 1;
 		row_upper_bound = row - 2;
 	}
 	
-	if(col_remainder === 0){
+	if(col_remainder === 0){ // check if our guess is in col 0, 3, or 6
 		col_lower_bound = col + 1;
 		col_upper_bound = col + 2;
-	} else if(col_remainder === 1) {
+	} else if(col_remainder === 1) { // check if our guess is in col 1, 4, or 7
 		col_lower_bound = col - 1;
 		col_upper_bound = col + 1;
-	} else if(col_remainder === 2) {
+	} else if(col_remainder === 2) { // check if our guess is in col 2, 5, or 8
 		col_lower_bound = col - 1;
 		col_upper_bound = col - 2;
 	}	
@@ -65,10 +102,8 @@ function isPossibleSol(num, row, col, board){
 	if( num === board[row_lower_bound][col_lower_bound] ||
 		num === board[row_lower_bound][col_upper_bound] ||	 
 		num === board[row_upper_bound][col_lower_bound] ||	 
-		num === board[row_upper_bound][col_upper_bound]	) {
-
+		num === board[row_upper_bound][col_upper_bound]	) { //check if guess already exists in its box
 			return false;	
 	}
-
-	return true;
+	return true; // returns true if all the tests pass, indicating that our guess is valid (for now)
 }
